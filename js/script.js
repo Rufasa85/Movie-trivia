66d651993c83e6c76ac20877e0787631f91728df
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-10-09 15:55:18 -0700

    converting the selected film to a version the API understands.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,3 @@
-var thisMovie;
 var possibleMovies = ["Star Wars: Episode IV - A New Hope", "Gone With the Wind", "citizen kane"];
 
 //trying to randomly select a movie to search
@@ -7,14 +6,16 @@ console.log(thisMovie);
 
 $(document).ready(function () {
 	console.log('linked!');
-
-	//fetching plot info for current question
-	// $.ajax('http://www.omdbapi.com/?t=gone+with+the+wind', {
-	// 	method:'GET',
-	// 	success:function(data) {
-	// 		thisMovie = data.Plot;
-	// 		console.log(thisMovie);
-	// 	},
-	// });
-	// console.log(thisMovie);
+	//making the term searchable in the API
+	thisMovie = thisMovie.split(' ').join('+');
+	console.log(thisMovie);
+//fetching plot info for current question
+	$.ajax('http://www.omdbapi.com/?t=gone+with+the+wind', {
+		method:'GET',
+		success:function(data) {
+			thisMovie = data.Plot;
+			console.log(thisMovie);
+		},
+	});
+	console.log(thisMovie);
 });
