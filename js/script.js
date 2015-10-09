60e318da0b31cf999d4b79ecdf14cfb62d9b27d7
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-10-09 15:49:21 -0700

    testing selection of a random title from an array of possibilities

--- js/script.js
+++ js/script.js
@@ -1,15 +1,20 @@
 var thisMovie;
+var possibleMovies = ["Star Wars: Episode IV - A New Hope", "Gone With the Wind", "citizen kane"];
+
+//trying to randomly select a movie to search
+var thisMovie = possibleMovies[Math.floor(Math.random() * 3)];
+console.log(thisMovie);
 
 $(document).ready(function () {
 	console.log('linked!');
 
 	//fetching plot info for current question
-	$.ajax('http://www.omdbapi.com/?t=gone+with+the+wind', {
-		method:'GET',
-		success:function(data) {
-			thisMovie = data.Plot;
-			console.log(thisMovie);
-		},
-	});
-	console.log(thisMovie);
+	// $.ajax('http://www.omdbapi.com/?t=gone+with+the+wind', {
+	// 	method:'GET',
+	// 	success:function(data) {
+	// 		thisMovie = data.Plot;
+	// 		console.log(thisMovie);
+	// 	},
+	// });
+	// console.log(thisMovie);
 });
