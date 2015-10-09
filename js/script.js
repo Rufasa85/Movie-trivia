9118f7404ffa3f9a84b8886485dc313d5b4fe389
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-10-09 15:37:33 -0700

    testing AJAX call

--- js/script.js
+++ js/script.js
@@ -1,3 +1,15 @@
+var thisMovie;
+
 $(document).ready(function () {
-console.log('linked!');
+	console.log('linked!');
+
+	//fetching plot info for current question
+	$.ajax('http://www.omdbapi.com/?t=gone+with+the+wind', {
+		method:'GET',
+		success:function(data) {
+			thisMovie = data.Plot;
+			console.log(thisMovie);
+		},
+	});
+	console.log(thisMovie);
 });
