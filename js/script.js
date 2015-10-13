e39329984e9cd2d6343e51bcd03f905a671ac548
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-10-13 09:31:56 -0700

    initial gh-pages files

---
+++ js/script.js
@@ -0,0 +1,200 @@
+var allMovies = ["Star Wars: Episode IV - A New Hope", "Gone With the Wind", "Citizen Kane",'Titanic', 'Avatar', "E.T. the Extra-Terrestrial", "The Lion King", "Jurassic Park", "Raiders of the Lost Ark", "Forrest Gump", "The Avengers", "Close Encounters of the Third Kind", "Grease", "Shrek", "Spider-Man", "Independence Day", 'Ghostbusters', "Beverly Hills Cop", "Home Alone", "Pirates of the Caribbean: The Curse of the Black Pearl", "Batman", "The Lord of the Rings: The Return of the King", "Finding Nemo", "The Sixth Sense", "Back to the Future", "Harry Potter and the Sorcerer's Stone", "Twister", "Superman", "Men in Black", "Transformers", "Mrs. Doubtfire", "Toy Story", "The Hunger Games", "Aladdin", "Iron Man", "Monsters Inc", "Frozen", "Ghost", "How the Grinch Stole Christmas", "The Terminator", "Top Gun", "The Matrix", "Saving Private Ryan", "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe", "Despicable Me", "Crocodile Dundee", "Gremlins", "Beauty and the Beast", "Armageddon", "An Officer and a Gentleman", "Alice in Wonderland", "Kramer vs. Kramer", "Inside Out", "Cast Away", "The Incredibles", "Furious 7", "American Sniper", "Rain Man", "The Fugitive", "Dances with Wolves", "3 Men and a Baby", "The Godfather", "Casablanca", "The Big Lebowski", "The NeverEnding Story", "Caddyshack", "Homeward Bound: The Incredible Journey", "Labyrinth", "Pulp Fiction", "Love Actually", "Notting Hill", "Goodfellas", "My Neighbor Totoro", "Spirited Away", "Anchorman: The Legend of Ron Burgundy", "The Shawshank Redemption", "Pleasantville", "Major League", "Kill Bill: Vol. 1", "Good Will Hunting", "The Sandlot", "Super Mario Bros.", "The Land Before Time", "Free Willy", "Lost in Translation", "Princess Mononoke", "Ponyo", "Edward Scissorhands", "The Jerk", "The Goonies", "The Lost Boys", "Fight Club", "The Life Aquatic with Steve Zissou", "Raging Bull", "I Heart Huckabees", "Lawrence of Arabia", "This Is Spinal Tap", "Thelma & Louise", "Alien", "Die Another Day", "There Will Be Blood", "The Royal Tenenbaums", "Jackie Brown", "Matilda", "Scream", "A Nightmare on Elm Street", "Mad Max", "Groundhog Day", "Django Unchained", "Stripes", "Guardians of the Galaxy", "The Silence of the Lambs", "Bill & Ted's Excellent Adventure","Amélie", "Legally Blonde" ];
+console.log(allMovies.length);
+var currentRound = 1;
+var possibleMovies = allMovies.slice(0,allMovies.length);
+var thisMoviePlot = '';
+var wrongAnswers = [];
+var thisMovie = '';
+var player1score = 0;
+var player2score = 0;
+var currentGuesser = '';
+var wrongGuesses = 0;
+// //populating the rest of the buttons with the wrong answers
+function addingWrongTitles(title) {
+	var thisButton = $($(".answers")[Math.floor(Math.random()*4)]);
+	if (thisButton.html() === '') {
+		thisButton.html(title);
+	}
+	else {
+		addingWrongTitles(title);
+	}
+};
+//turning title clicks into a function i can call inside of buzzingIn
+function allowingClicksOnTitles () {
+	$('.answers').click(function(){
+		if($(this).attr('id') === 'right') {
+			$(this).addClass('btn-success');
+			//changing button styling so they appear unselectable
+			$('.answers').css('opacity','0.5');
+			//checking who is guessing, updating correct score
+			if (currentGuesser === 'Player 1'){
+				player1score ++;
+				console.log(player1score);
+				$('#p1Score').html(player1score);
+			}
+			else if (currentGuesser === 'Player 2'){
+				player2score ++;
+				console.log(player2score);
+				$('#p2Score').html(player2score);
+			}
+			$('.answers').off('click');
+			$('#nextRound').show();
+		}
+		else {
+			$(this).addClass('btn-danger');
+			//removing click event from this title, so it cannot be clicked twice
+			$(this).off('click');
+			//allowing other player to guess if first guess is wrong
+			wrongGuesses++;
+			if (wrongGuesses <= 1) {
+				if ( currentGuesser === 'Player 1') {
+					swal({
+						title:'Icorrect!',
+						text:' Player 2, your turn!!',
+						type:'error',
+					});
+					currentGuesser = 'Player 2';
+				}
+				else if (currentGuesser === 'Player 2') {
+					swal({
+						title:'Incorrect!',
+						text:'Player 1, your turn!',
+						type:'error',
+					});
+					currentGuesser = 'Player 1'
+				} 
+			}
+			else {
+				//changing button styling so they appear unselectable
+				$('.answers').css('opacity','0.5');
+				$('.answers').off('click');
+				$('#nextRound').show();
+			}
+		}
+		//checking for winner, revealing winner and hiding everything else
+		if (player1score === 5 || player2score === 5){
+			if (player1score === 5){
+				$('#winningPlayer').html('Player 1 wins!')
+			}
+			else {
+				$('#winningPlayer').html('Player 2 wins!');
+			}
+			$('#nextRound').hide();
+			$('#winner').show();
+			$('.gameboard').hide();
+			$('#plot').html('<img src = http://www.cliparthut.com/clip-arts/567/oscar-awards-clip-art-567814.png>')
+		} 
+	});
+}
+//turning movie selection into a function
+function generatingCurrentRoundTitles() {
+	wrongAnswers = [];
+	//trying to randomly select a movie to search
+	thisMovie = possibleMovies[Math.floor(Math.random() * possibleMovies.length)];
+	//removing selected film from possible answers
+	possibleMovies.splice(possibleMovies.indexOf(thisMovie), 1);
+	//generating other possible titles
+	for (var i = 0; i<3; i++) {
+		var thisWrongMovie = possibleMovies[Math.floor(Math.random() * possibleMovies.length)];
+		possibleMovies.splice(possibleMovies.indexOf(thisWrongMovie), 1);
+		wrongAnswers.push(thisWrongMovie);
+	}
+};
+//placing titles in random spots on the board
+function populatingCurrentTitlesToBoard () {
+	//resetting button opacity
+	$('.answers').css('opacity','1');
+	//adding correct title to random spot on the screen
+	$($(".answers")[Math.floor(Math.random()*4)]).html(thisMovie).attr('id','right');
+		//making the term searchable in the API
+	thisMovie = thisMovie.split(' ').join('+');
+	console.log(thisMovie);
+	wrongAnswers.forEach(addingWrongTitles);
+};
+//setting up each individual round
+function generatingRound() {
+	console.log(currentRound);
+	$('#round').html(currentRound);
+	generatingCurrentRoundTitles();
+	populatingCurrentTitlesToBoard();
+	//retrieving movie plot from OMDB API
+	$.ajax('http://www.omdbapi.com/?t=' + thisMovie , {
+		method:'GET',
+		success:function(data) {
+			thisMoviePlot = data.Plot;
+			console.log(thisMoviePlot);
+			//displaying current plot on screen
+			$('#plot').html(thisMoviePlot);
+		},
+	});
+	//iterating round number
+	currentRound++;
+	buzzingIn();
+};
+//testing keypress for each player!
+function buzzingIn() {
+	$(document).keydown(function( event ) {
+		if ( event.which == 81 ) {
+			//disabling keydown after buzz in
+			$(this).off('keydown');
+			swal({
+				title:'player 1',
+				text:'make your guess!',
+				type:'warning',
+			});
+			//defining who buzzed in
+			currentGuesser = 'Player 1';
+		}
+		else if(event.which == 77) {
+			$(this).off('keydown');
+			swal({
+				title:'player 2',
+				text:'make your guess!',
+				type:'warning',
+			});
+			//defining who buzzed in
+			currentGuesser = 'Player 2';
+		}
+		//setting other click listeners to only happen after buzzing
+		allowingClicksOnTitles();
+	});
+}
+
+$(document).ready(function () {
+	//initializing view
+	$('#nextRound').hide();
+	$('#winner').hide();
+	$('.gameboard').hide();
+	$('.scores').hide();
+	generatingRound(currentRound);
+	//showing gameboard when start button is clicked
+	$('#start-btn').click(function(){
+		$('.gameboard').show();
+		$('.scores').show();
+		$('.splash').hide();
+	})
+	//resetting board for next question
+	$('#nextRound').click(function(){
+		wrongGuesses = 0;
+		$('.answers').removeClass('btn-danger').removeClass('btn-success').html('').removeAttr('id');
+		generatingRound(currentRound);
+		$('#nextRound').hide();
+	})
+	//resetting game when reset button is clicked
+	$('#newGame').click(function(){
+		$('#nextRound').hide();
+		$('#winner').hide();
+		$('.gameboard').hide();
+		$('.scores').hide();
+		$('.splash').show();
+		currentRound = 1;
+		player1score = 0;
+		player2score = 0;
+		wrongGuesses = 0;
+		possibleMovies = allMovies.slice(0,allMovies.length);
+		$('#p2Score').html(player2score);
+		$('#p1Score').html(player1score);
+		$('.answers').removeClass('btn-danger').removeClass('btn-success').html('').removeAttr('id');
+		generatingRound(currentRound);
+	});
+});
---
+++ js/testingFunctions.js
@@ -0,0 +1,34 @@
+//turning movie selection into a function
+function generatingCurrentRoundTitles() {
+	//trying to randomly select a movie to search
+	var thisMovie = possibleMovies[Math.floor(Math.random() * possibleMovies.length)];
+	//removing selected film from possible answers
+	possibleMovies.splice(possibleMovies.indexOf(thisMovie), 1);
+	//generating other possible titles
+	for (var i = 0; i<3; i++) {
+		var thisWrongMovie = possibleMovies[Math.floor(Math.random() * possibleMovies.length)];
+		possibleMovies.splice(possibleMovies.indexOf(thisWrongMovie), 1);
+		wrongAnswers.push(thisWrongMovie);
+	}
+};
+
+function populatingCurrentTitlesToBoard () {
+	//adding correct title to random spot on the screen
+	$($(".answers")[Math.floor(Math.random()*4)]).html(thisMovie).attr('id','right');
+	//changing clolor of button on click
+	$('#right').click(function() {
+		$(this).addClass('btn-success');
+		$('.answers').off('click');
+		$('#nextRound').show();
+	});
+	wrongAnswers.forEach(addingWrongTitles);
+};
+
+function generatingRound(round) {
+	generatingCurrentRoundTitles();
+	populatingCurrentTitlesToBoard();
+	round++;
+	if (round === 11) {
+		console.log('game over!')
+	} 
+};
